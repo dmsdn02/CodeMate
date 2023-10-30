54b3803327947e14e1c6ea52f69b2b0fd21532f7
eunwoo <202245047@itc.ac.kr>
2023-10-30 14:25:29 +0900

    feat : 로그인페이지 디자인 수정 및 이미지 생성

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -17,66 +17,54 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFCEA8A), // 배경색 설정
+
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 300, // 네모박스의 가로길이 조절 가능
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.grey[200], // 배경색 설정
-              ),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: '이메일',
-                    ),
+        child: Container(
+          width: 320, // 네모박스의 너비 설정
+          padding: EdgeInsets.all(7), // 네모박스 내부 패딩 설정
+          decoration: BoxDecoration(
+            color: Colors.white, // 네모박스 배경색 설정
+            borderRadius: BorderRadius.circular(20), // 둥근 네모박스를 위한 BorderRadius 설정
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              // 이메일 입력란
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: '이메일',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: InputBorder.none,
+                    prefixIcon: Icon(Icons.email), // 아이콘 추가
                   ),
-                  SizedBox(height: 10),
-                  TextField(
-                    obscureText: true, // 비밀번호 필드로 설정
-                    decoration: InputDecoration(
-                      hintText: '비밀번호',
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      // 로그인 버튼 클릭 시 수행할 동작 설정
-                    },
-                    child: Text('로그인'),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    // 회원가입 버튼 클릭 시 수행할 동작 설정
-                  },
-                  child: Text(
-                    '회원가입',
-                    style: TextStyle(fontSize: 12),
+                  style: TextStyle(
+                    color: Colors.grey, // 글자 색상 지정
                   ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    // 비밀번호 찾기 버튼 클릭 시 수행할 동작 설정
-                  },
-                  child: Text(
-                    '비밀번호 찾기',
-                    style: TextStyle(fontSize: 12),
+              ),
+              Divider(height: 20, color: Colors.grey[400]), // 가로선 추가
+              // 비밀번호 입력란
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: '비밀번호',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: InputBorder.none,
+                    prefixIcon: Icon(Icons.lock), // 아이콘 추가
+                  ),
+                  style: TextStyle(
+                    color: Colors.grey[50], // 글자 색상 지정
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
